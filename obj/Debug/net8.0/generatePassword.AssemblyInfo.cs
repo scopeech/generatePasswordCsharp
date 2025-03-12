6c888272b071f4b815a6cc57d23875b44d8f5cee
scopeech <max260996@gmail.com>
2025-03-13 02:20:51 +0600

    Полностью чистый код

--- obj/Debug/net8.0/generatePassword.AssemblyInfo.cs
+++ obj/Debug/net8.0/generatePassword.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("generatePassword")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8944d0066cbc8400ba62dbc2681fe00cf4c342c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55886cb2b113c775282291e9b6ccd86fc5369409")]
 [assembly: System.Reflection.AssemblyProductAttribute("generatePassword")]
 [assembly: System.Reflection.AssemblyTitleAttribute("generatePassword")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
